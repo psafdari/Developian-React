1327b7309ee65df4557b64ab8d4e56ceed66ecf6
psafdari <psafdari@GMAIL.COM>
2025-01-06 01:01:26 +1100

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <h1>Hello everyone</h1>
     </div>
   );
-=======
-<h1> Hello Everyone</h1>
-    </div>
->>>>>>> e072ce61076b457648408cada3a1320f0bb4b400
 }
 
 export default App;
